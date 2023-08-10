951598dcbaff7432e9f0a0e423f42996fee95d51
Amina Seye <aminaseye2@icloud.com>
2023-08-10 23:25:08 +0200

    main

--- dist/main.js
+++ dist/main.js
@@ -7,27 +7,77 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { getCharacters } from "./utils/API.js";
+import { getSingleCharacter, getEpisodes } from "./utils/API.js";
 window.addEventListener("load", init);
+const mainContainer = document.querySelector("#mainContainer");
+const asideEpisode = document.querySelector("#asideEpisode");
 function init() {
+    addEpisodes();
+}
+function addEpisodes() {
     return __awaiter(this, void 0, void 0, function* () {
-        const characterList = document.querySelector("#characterList");
-        const characters = yield getCharacters();
-        characters.forEach((char) => {
-            const characterCard = document.createElement("div");
-            const characterCardTitle = document.createElement("h3");
-            const characterCardTitleTxt = document.createTextNode(char.name);
-            characterCardTitle.appendChild(characterCardTitleTxt);
-            const characterImg = document.createElement("img");
-            characterImg.src = char.image;
-            characterImg.alt = `${char.name} Image `;
-            characterImg.addEventListener("click", sayAlt);
-            characterCard.appendChild(characterCardTitle);
-            characterCard.appendChild(characterImg);
-            characterList.appendChild(characterCard);
+        const episodes = yield getEpisodes();
+        episodes.forEach((episode) => {
+            const episodeLink = document.createElement("button");
+            episodeLink.innerText = episode.name;
+            episodeLink.addEventListener("click", () => {
+                openEpisodeContainer(episode);
+            });
+            asideEpisode.appendChild(episodeLink);
         });
-        function sayAlt(event) {
-            console.log(this.alt);
-        }
+    });
+}
+function openEpisodeContainer(epi) {
+    mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.replaceChildren();
+    const episode = epi;
+    const episodeContainer = document.createElement("div");
+    const name = document.createElement("h1");
+    const airDate = document.createElement("p");
+    const episodeCode = document.createElement("h1");
+    const characterButtonsContainer = document.createElement("div");
+    const characters = episode.characters;
+    name.innerText = episode.name;
+    airDate.innerText = episode.air_date;
+    episodeCode.innerText = `${episode.id}`;
+    characters.forEach((char) => {
+        createCharacterCard(char, characterButtonsContainer);
+    });
+    episodeContainer.appendChild(name);
+    episodeContainer.appendChild(airDate);
+    episodeContainer.appendChild(episodeCode);
+    episodeContainer.appendChild(characterButtonsContainer);
+    mainContainer.appendChild(episodeContainer);
+}
+function createCharacterCard(char, container) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const singleCharacter = yield getSingleCharacter(char);
+        const characterLink = document.createElement("button");
+        const characterContainer = document.createElement("div");
+        characterLink.addEventListener("click", () => {
+            openCharacterModal(char, characterContainer);
+        });
+        characterLink.innerText = singleCharacter.name;
+        const characterStatus = document.createElement("p");
+        characterStatus.innerText = singleCharacter.status;
+        const characterSpecies = document.createElement("p");
+        characterSpecies.innerText = singleCharacter.species;
+        const characterGender = document.createElement("p");
+        characterGender.innerText = singleCharacter.gender;
+        const characterImg = document.createElement("img");
+        characterImg.innerText = singleCharacter.image;
+        container.appendChild(characterLink);
+        characterLink.appendChild(characterStatus);
+        characterLink.appendChild(characterGender);
+        characterLink.appendChild(characterSpecies);
+        characterLink.appendChild(characterImg);
+    });
+}
+function openCharacterModal(char, container) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const name = document.createElement("p");
+        const openModal = document.createElement("modal");
+        openModal.addEventListener("click", () => {
+            console.log("modal abierto");
+        });
     });
 }
